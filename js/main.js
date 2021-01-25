e1549dc3fdceb6c77b06d64d9fdda56ee4fa7931
Mateusz <mati8765432a@gmail.com>
2021-01-25 16:00:26 +0100

    final changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 const menu = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.sidebar')
 const readMore = document.querySelector('.btn')
 const showMore = document.querySelector('.intro__more-info')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('hamburger--active');
-    nav.classList.toggle('slider');
+    nav.classList.toggle('change');
 });
 
 readMore.addEventListener('click', () =>{
     showMore.classList.toggle('show-more')
 });
 
+$('div[data-scroll]').click(function (e) {
+    e.preventDefault();
+    var offset = 10;
+    var target = ('#' + $(this).data('scroll'));
+    var $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - offset
+    }, 0, 'swing');
+}); 
